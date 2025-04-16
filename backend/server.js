462b0fcae319cf7c72dec9cc8c30269d65ae2c2c
kailyncleere <79721722+kailyncleere@users.noreply.github.com>
2025-04-16 12:41:57 -0500

    connect sql api

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,34 @@
 const express = require('express'); // Import the express module
 const app = express(); // Create an instance of express
 
+const mysql = require('mysql2'); // Import the mysql2 module (latest version of mysql)
+const db = mysql.createConnection({ // Create a connection to the database
+    host: 'localhost', // Database hosted on localhost
+    user: 'root', // Database username is root due to mysql being installed locally on the root
+    password: 'Calleycat1', // Password for the database
+    database: 'forum_app' // Database name in mysql
+
+});
+
+db.connect(err => { // Connect to the database and throw an error if it fails
+    if (err) {
+        throw err;
+    }
+    console.log('Successfully Connected To The Database!'); // Console logs in vscode to confirm connection to the database is successful
+})
+
+
 app.get('/getServerInfo', (request, response) => { 
     response.send('Hello From Express!');
+})
+
+app.get('/getUsers', (request, response) => {
+    db.query('select * from user_tbl', (err, rows) => { // selects all data from the user_tbl in the mysql database and throws an error if it fails
+        if (err) {
+            throw err;
+        }
+        response.send(rows); // Send the rows data as a response to the client
+    })
 })
 
 app.listen(3000, () => console.log('Server Is Running on 3000'));
